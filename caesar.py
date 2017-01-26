a1bcc4abe6783c38dd8d6ad0276eaae3d3829e77
Jennifer Kocher <jenkocher@gmail.com>
2017-01-26 07:26:59 -0600

    Initial commit. Dummy code for encryption and two helper functions. (Code is on other laptop.)

---
+++ caesar.py
@@ -0,0 +1,11 @@
+def alphabet_position(letter):
+    m = "This is the alphabet_position function."
+    return m
+
+def rotate_char(char, rot):
+    m = "This is the rotate_char function."
+    return m
+
+def encrypt(message, rot):
+    m = "This is the encrypt function."
+    return m
