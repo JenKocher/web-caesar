a64a49cd4a284ceb280a338ddc23749c37b5dc91
Jennifer Kocher <jenkocher@gmail.com>
2017-01-26 18:40:18 -0600

    Consolidated repeated code into single function

--- caesar.py
+++ caesar.py
@@ -1,11 +1,30 @@
 def alphabet_position(letter):
-    m = "This is the alphabet_position function."
-    return m
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    lowercase = letter.lower()
+    idx = alphabet.index(lowercase)
+    return idx
 
-def rotate_char(char, rot):
-    m = "This is the rotate_char function."
-    return m
 
-def encrypt(message, rot):
-    m = "This is the encrypt function."
-    return m
+def rotate_character(char, rot):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    if char.lower() in alphabet:
+        if char.upper() == char:
+            uppercase = True
+        else:
+            uppercase = False
+        oldpos = alphabet_position(char)
+        newpos = oldpos + rot
+        if newpos > 25:
+            newpos = newpos % 26
+        newchar = alphabet[newpos]
+        if uppercase:
+            newchar = newchar.upper()
+    else:
+        newchar = char
+    return newchar
+
+def encrypt(message, rotation):
+    newmessage = ''
+    for char in message:
+        newmessage = newmessage + rotate_character(char, rotation)
+    return newmessage
